5e32eabc64d6b4e5d3b9d96d09b8b94561634f56
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-07-30 23:52:32 +0700

    pageTitle

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config();
 const express = require('express');
 const route = require("./routes/client/index.route");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
