aef2a5d3ca05a8eaca7030e77afd52c312f6ed0b
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-07-31 23:40:10 +0700

    Embed bootstrap and static files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
 
