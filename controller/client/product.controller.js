40e674929741d6e96f31c52f9a4ee62215e7868a
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-08-02 23:29:39 +0700

    create products display on fontend

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,14 +1,21 @@
 const Product = require("../../models/product.model.js")
 
 module.exports.index = async (req, res) => {
-    const product = await Product.find({
+    const products = await Product.find({
         status: "active",
         deleted: false
     });
-    console.log(product);
+    console.log(products);
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
+        return item;
+    });
+    console.log(newProducts);
 
 
     res.render("client/pages/products/index", {
-        pageTitle: "Products List"
+        pageTitle: "Products List",
+        products: products
     });
 }
