5b306a6b6a3fad909d0415e305e1d8a28f03caee
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-08-09 14:59:24 +0700

    1.2 Xay Dung layout chung

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const database = require('./config/database')
+const systemConfig = require('./config/system');
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route")
 database.connect();
@@ -12,6 +13,9 @@ const port = process.env.PORT;
 app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// app local 
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 //route
 route(app);
