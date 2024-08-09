678b1be4d4ccc4a3b35a2854db0e1c15e8647518
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-08-09 23:46:24 +0700

    Lấy data in ra giao diện

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,7 +1,15 @@
 // [GET] /admin/products 
-module.exports.index = (req, res) => {
+
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    console.log(products);
     res.render("admin/pages/products/index", {
-        pageTitle: "Products List"
+        pageTitle: "Products List",
+        products: products
     }
     );
 }
