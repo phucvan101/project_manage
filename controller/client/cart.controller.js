e3b6ce473cdad8ae7f0196ba665d9c24464d91ca
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-12-25 16:13:50 +0700

    display cart page

--- controller/client/cart.controller.js
+++ controller/client/cart.controller.js
@@ -1,4 +1,32 @@
 const Cart = require("../../models/cart.model")
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId,
+    })
+    if (cart.products.length > 0) {
+        for (const item of cart.products) {
+            const productId = item.product_id;
+            const productInfo = await Product.findOne({
+                _id: productId,
+            }).select("title thumbnail slug price discountPercentage");
+            productInfo.priceNew = productsHelper.priceNewProduct(productInfo)
+            item.productInfo = productInfo;
+            item.totalItemPrice = productInfo.priceNew * item.quantity;
+        }
+    }
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalItemPrice, 0);
+    res.render('client/pages/cart/index', {
+        pageTitle: "Cart",
+        cartDetail: cart
+    });
+}
+
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
     const productId = req.params.productId;
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controller/client/cart.controller')
 
+router.get('/', controller.index);
 
 router.post('/add/:productId', controller.addPost);
 
