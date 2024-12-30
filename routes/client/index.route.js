1c8b3b2cd3758aca38e5713a712c6b410946e3a5
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2024-12-30 10:10:16 +0700

    page success order

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 const cartMiddleware = require("../../middlewares/client/cart.middlewares")
 const searchRoutes = require("./search.route")
 const cartRoutes = require("./cart.route")
+const checkoutRoutes = require("./checkout.route")
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category) // tối ưu sử dụng đỡ phải gọi lại 
@@ -12,4 +13,5 @@ module.exports = (app) => {
     app.use("/products", productRoute);
     app.use("/search", searchRoutes);
     app.use("/cart", cartRoutes);
+    app.use("/checkout", checkoutRoutes);
 }
